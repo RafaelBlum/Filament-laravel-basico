d730ecb148d39c1483d1470b32dbf583ac221d58
RafaelBlum <Rafaelblum_digital@hotmail.com>
2023-11-02 10:44:58 -0300

    feat: first commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -42,4 +44,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+//    public function canAccessPanel(Panel $panel): bool
+//    {
+//        return str_ends_with($this->email, '@hotmail.com') && $this->hasVerifiedEmail();
+//    }
 }
